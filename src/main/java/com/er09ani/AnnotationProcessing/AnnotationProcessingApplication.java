44a62a301eeaf58b29d1a13b573858c4776fc33e
Aniruddha Naik <er09ani@gmail.com>
2025-12-01 12:27:33 +0530

    project setup

---
+++ src/main/java/com/er09ani/AnnotationProcessing/AnnotationProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.er09ani.AnnotationProcessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/er09ani/AnnotationProcessing/AnnotationProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.er09ani.AnnotationProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
